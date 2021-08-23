6f8d7b0189170cc032c1279b31d8a2e5b5d51ce7
HiDef888 <38380243+HiDef888@users.noreply.github.com>
2021-08-22 20:40:38 -0500

    Update dns-test-up.sh

--- dns-test-up.sh
+++ dns-test-up.sh
@@ -4,15 +4,15 @@ HOST="8.8.8.8"
 WAITFOR=3
 TIMES=2
 
+ONLINE=0
+
 ping $HOST -c $TIMES -i $WAITFOR &> /dev/null
 pingReturn=$?
 
 if [ $pingReturn -eq 0 ]; then
     # It works
-    echo "Success!!!"
     exit 0
 else
     # No access
-    echo "Fail"
     exit 1
 fi
