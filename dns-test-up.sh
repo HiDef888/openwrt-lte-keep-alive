5503a270cec421b638a828077a19e8e6ba22d65c
HiDef888 <38380243+HiDef888@users.noreply.github.com>
2021-08-22 20:36:03 -0500

    Create dns-test-up.sh

---
+++ dns-test-up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+HOST="8.8.8.8"
+WAITFOR=3
+TIMES=2
+
+ping $HOST -c $TIMES -i $WAITFOR &> /dev/null
+pingReturn=$?
+
+if [ $pingReturn -eq 0 ]; then
+    # It works
+    echo "Success!!!"
+    exit 0
+else
+    # No access
+    echo "Fail"
+    exit 1
+fi
